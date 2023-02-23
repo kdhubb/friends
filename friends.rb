53473dd8a441929874d1dbc52d33a95205d55da1
K.D. Hubbard <hubbard.kd@gmail.com>
2023-02-23 15:25:24 -0700

    Add places to visit array.

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Chandler", "Rachael", "Monica", "Ross", "Phoebe"]
 puts arrays[3]
 puts arrays[4]
 puts arrays[0]
+
+places_to_visit = ["Antarctica", "New Zealand", "Scotland", "Central America", "Alaska"]
+places_to_visit.push("Brazil")
+puts places_to_visit  
